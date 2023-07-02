3dc31b875d797660a72b6f9f2348dd6c9f2bb835
猫仔嘎嘎可爱 <1647741466@qq.com>
2023-07-03 00:31:16 +0800

    第三期结束

---
+++ config/routes.ts
@@ -0,0 +1,31 @@
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
+  },
+
+  {//临时重定向到主页显示
+    path: '/',
+    redirect: '/add_chart'
+  },
+  {
+    path: '/add_chart',
+    name: '智能分析',
+    icon: 'barChart',
+    component: './AddChart'
+  },
+  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+  {
+    path: '/admin',
+    name: '管理页',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,37 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+const Footer: React.FC = () => {
+  const defaultMessage = 'SHI超有钱集团出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'SHI LX BI',
+          title: 'SHI LX BI',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'SHI LX BI',
+          title: 'SHI LX BI',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
